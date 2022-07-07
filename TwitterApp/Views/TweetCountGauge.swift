7253e8f37d849d6085974cf91b084aa156824b4b
Mohammad Azam <azamsharp@gmail.com>
2022-07-07 13:07:51 -0500

    added TweetCountGauage

---
+++ TwitterApp/Views/TweetCountGauge.swift
@@ -0,0 +1,20 @@
+//
+//  TweetCountGauge.swift
+//  TwitterApp
+//
+//  Created by Mohammad Azam on 7/7/22.
+//
+
+import SwiftUI
+
+struct TweetCountGauge: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TweetCountGauge_Previews: PreviewProvider {
+    static var previews: some View {
+        TweetCountGauge()
+    }
+}
