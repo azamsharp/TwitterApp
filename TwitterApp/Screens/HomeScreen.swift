1e1d799104a2956a02f4f7c75e5d9a147eeb0dcc
Mohammad Azam <azamsharp@gmail.com>
2022-07-08 14:24:47 -0500

    Twitter Feed Screen

---
+++ TwitterApp/Screens/HomeScreen.swift
@@ -0,0 +1,60 @@
+//
+//  TimelineScreen.swift
+//  TwitterApp
+//
+//  Created by Mohammad Azam on 7/4/22.
+//
+
+import SwiftUI
+
+struct HomeScreen: View {
+    
+    @State private var isPresented: Bool = false
+    
+    var body: some View {
+        ZStack(alignment: .bottomTrailing) {
+            TabView {
+                TwitterFeedScreen()
+                    .tabItem {
+                        Label("Home", systemImage: "house")
+                    }
+                
+                Text("Search")
+                    .tabItem {
+                        Label("Search", systemImage: "magnifyingglass")
+                    }
+                
+                Text("Notifications")
+                    .tabItem {
+                        Label("Notifications", systemImage: "bell")
+                    }
+                
+                Text("Direct Message")
+                    .tabItem {
+                        Label("Direct Message", systemImage: "envelope")
+                    }
+            }
+            
+            Button {
+                // add tweet button
+                isPresented = true
+            } label: {
+                Image(systemName: "plus.circle.fill")
+                    .foregroundColor(.blue)
+                    .font(.system(size: 50))
+                    
+                    
+            }.padding([.bottom], 60)
+                .padding([.trailing], 20)
+
+        }.sheet(isPresented: $isPresented) {
+            ComposeTweetScreen()
+        }
+    }
+}
+
+struct HomeScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeScreen()
+    }
+}
